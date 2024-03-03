b0eb29e54714bafdab6cf304686f78a13dc2761f
CarlMurray <carl.j.murray@gmail.com>
2024-03-03 19:35:16 +0000

    Initialise

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookshelfAPI.Controllers
+{
+    [Route("api/books")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly ILogger<BooksController> _logger;
+
+        public BooksController(ILogger<BooksController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book()
+                {
+                    Id = 1, Author = "Charles Duhigg", Name = "Habit", Description = "How to form life changing habits."
+                },
+                new Book()
+                {
+                    Id = 2, Author = "Jaime Levy", Name = "UX Strategy", Description = "A guide to UX strategy"
+                },
+                new Book()
+                {
+                    Id = 3, Author = "David Travis", Name = "Think Like a UX Researcher",
+                    Description = "How to survive as a UXR."
+                }
+            };
+        }
+    }
+}
